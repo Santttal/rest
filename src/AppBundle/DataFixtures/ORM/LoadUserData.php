6db8d830afbab12092533b333811f10161fcda59
Гафаров Артур <a.gafarov@elama.ru>
2015-05-13 21:50:49 +0300

    loadUserData loadGroupData UserRepository

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -14,8 +14,14 @@ class LoadUserData implements FixtureInterface
         $userAdmin->setAge(27);
         $userAdmin->setLogin('Artur');
         $userAdmin->setPassword('megapass');
-
         $manager->persist($userAdmin);
+
+        $userAndrey = new User();
+        $userAndrey->setAge(27);
+        $userAndrey->setLogin('Andrey');
+        $userAndrey->setPassword('santtal');
+        $manager->persist($userAndrey);
+
         $manager->flush();
     }
 }
